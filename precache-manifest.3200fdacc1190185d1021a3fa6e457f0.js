c77e1327b69f32770768dff8a3efce373a578743
Min Su Kim <kim97minsu@gmail.com>
2020-03-04 21:51:59 -0800

    Updates

---
+++ precache-manifest.3200fdacc1190185d1021a3fa6e457f0.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edb6445b211402aebeebdfb21d44843f",
+    "url": "/Bike-Shop/index.html"
+  },
+  {
+    "revision": "962b7077821209e1bd6d",
+    "url": "/Bike-Shop/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "397d0fbbce301e4b375a",
+    "url": "/Bike-Shop/static/css/main.079a64cf.chunk.css"
+  },
+  {
+    "revision": "962b7077821209e1bd6d",
+    "url": "/Bike-Shop/static/js/2.93c9cdac.chunk.js"
+  },
+  {
+    "revision": "d342664d578042145f5010e743958e89",
+    "url": "/Bike-Shop/static/js/2.93c9cdac.chunk.js.LICENSE"
+  },
+  {
+    "revision": "397d0fbbce301e4b375a",
+    "url": "/Bike-Shop/static/js/main.d3552b5c.chunk.js"
+  },
+  {
+    "revision": "5e3e4d9fa870be8e5fb3",
+    "url": "/Bike-Shop/static/js/runtime-main.9b8f8b92.js"
+  },
+  {
+    "revision": "0cb7fd88aed0875a1ec6ac8308b065a1",
+    "url": "/Bike-Shop/static/media/bikeview.0cb7fd88.jpg"
+  },
+  {
+    "revision": "6a364ec29b2bb612c5622fbd2f457cb0",
+    "url": "/Bike-Shop/static/media/cannondale.6a364ec2.jpeg"
+  },
+  {
+    "revision": "4ff2b7316e4908514f0e9e25be6acc1a",
+    "url": "/Bike-Shop/static/media/footerimage.4ff2b731.png"
+  },
+  {
+    "revision": "d361c615bf3983429477749bdca2a76d",
+    "url": "/Bike-Shop/static/media/riding.d361c615.mp4"
+  },
+  {
+    "revision": "b337033e7bfab69d4a27ea5fadb23a7c",
+    "url": "/Bike-Shop/static/media/shoppingcart.b337033e.png"
+  },
+  {
+    "revision": "5886859c4d915d31b36a7e4341dff4d5",
+    "url": "/Bike-Shop/static/media/specialized.5886859c.png"
+  },
+  {
+    "revision": "2573a50c79b919a4f62d79f3fb3348b7",
+    "url": "/Bike-Shop/static/media/sworks.2573a50c.jpg"
+  },
+  {
+    "revision": "afd0de21819b87ef9fe8015e4702e8b0",
+    "url": "/Bike-Shop/static/media/trek.afd0de21.png"
+  }
+]);
