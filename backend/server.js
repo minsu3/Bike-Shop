bbc9eef5f4d997a2ba2abf8e58bf7cf024f18e49
Min Su Kim <kim97minsu@gmail.com>
2020-05-26 22:41:21 -0700

    changed server port number

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ let app = express();
 // Middleware
 app.use(express.json());
 
-const port = 9000;
+const port = 8080;
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
